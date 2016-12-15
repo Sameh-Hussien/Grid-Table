6fb6ddd85facf7a527a93a89e3c6e69e74df0f7a
Sameh-Hussien <eng.sameh.hussien.2010@gmail.com>
2016-12-15 10:51:08 +0100

    Delete VerticalPartition.cpp

--- Source/VerticalPartition.cpp
+++
@@ -1,7 +0,0 @@
-#include "VerticalPartition.h"
-#include <inttypes.h>
-
-Register<Partition, VerticalPartition, uint64_t&, uint64_t&, uint64_t&, bool& > vPartition("VerticalPartition");
-
-VerticalPartition::VerticalPartition(uint64_t partitionID, uint64_t numRows, uint64_t numCols, bool storageLayout) :
-    Partition(partitionID, numRows, numCols, storageLayout) {};
