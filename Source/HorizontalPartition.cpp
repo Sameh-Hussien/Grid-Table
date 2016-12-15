732103c23b9ccba6cd8e1747299509f9aff4f3d8
Sameh-Hussien <eng.sameh.hussien.2010@gmail.com>
2016-12-15 10:51:00 +0100

    Delete HorizontalPartition.cpp

--- Source/HorizontalPartition.cpp
+++
@@ -1,7 +0,0 @@
-#include"HorizontalPartition.h"
-#include <inttypes.h>
-
-Register<Partition, HorizontalPartition, uint64_t&, uint64_t&, uint64_t&, bool& > hPartition("HorizontalPartition");
-
-HorizontalPartition::HorizontalPartition(uint64_t partitionID, uint64_t numRows, uint64_t numCols, bool storageLayout) :
-    Partition(partitionID, numRows, numCols, storageLayout) {};
